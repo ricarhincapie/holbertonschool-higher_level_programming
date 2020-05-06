31b82179719cc6b3f8b534f93780069f05ac13a9
Ricardo Hincapie <1516@holbertonschool.com>
2020-05-06 22:46:07 +0000

    Add task #8 correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        lenght = len(sentence)
-        first = sentence
-        return(lenght, first)
- 
+	lenght = len(sentence)
+	if lenght == 0:
+		return 0, None
+	else:
+		return lenght, sentence[0]
+		
