77ebc86a58101d5653080e080673157c6e983d6a
Ricardo Hincapie <1516@holbertonschool.com>
2020-05-07 00:23:39 +0000

    Add task #10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+def divisible_by_2(my_list=[]):
+        if not my_list:
+                return
+        new_list = my_list.copy()
+
+        for i in range(len(my_list)):
+                if my_list[i] % 2 is 0:
+                        new_list[i] = True
+                else:
+                        new_list[i] = False
+        return new_list
