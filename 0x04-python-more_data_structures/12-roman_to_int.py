7a14ed96d1ec06dcbb1cfa9b3a2225f1390adb23
Ricardo Hincapie <1516@holbertonschool.com>
2020-05-09 01:17:10 +0000

    Add task #12 corrections

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     from functools import reduce
+    if not roman_string or type(roman_string) is not str:
+        return 0
 
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum_list = []
