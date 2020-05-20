ecebf4eaea18f765035b7cdab73dc9765c28fba6
Ricardo Hincapie <1516@holbertonschool.com>
2020-05-20 02:57:10 +0000

    Add task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square():
+        """Square class
+        """
+
+        def __init__(self, size):
+                """Initialization of class square
+
+                Args:
+                        size: size of square.
+                """
+                self.__size = size
