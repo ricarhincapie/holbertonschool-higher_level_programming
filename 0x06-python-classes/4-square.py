533f99934875c9977565d751df7a4baed2f8c465
Ricardo Hincapie <1516@holbertonschool.com>
2020-05-20 03:45:32 +0000

    Add task #4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square():
+        """Square class
+        """
+
+        def __init__(self, size=0):
+                """Initialization of class square
+
+                Args:
+                        size: size of square (optional).
+                """
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
+                self.__size = size
+
+        def area(self):
+                """Method to return area of Square class
+
+                Returns:
+                        Area of the square
+                """
+                return self.__size ** 2
+
+        @property
+        def size(self):
+                """Getter to return size of Square class
+                """
+                return self.__size
+
+        @size.setter
+        def size(self, size):
+                """Setter size of square
+                """
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
+                self.__size = size
