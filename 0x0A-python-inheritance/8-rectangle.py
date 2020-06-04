479e8c0be9f4e2604e2490cdb260ee83578b6edf
Ricardo Hincapie <1516@holbertonschool.com>
2020-06-04 21:57:37 +0000

    Add task #8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module creates a function"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry
+    """
+    def area(self):
+            """
+            Area
+                    :param self:
+            """
+            raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+            """
+            Integer_validator
+                    :param self:
+                    :param name:
+                    :param value:
+            """
+            if type(value) != int:
+                    raise TypeError("{} must be an integer".format(name))
+
+            if value <= 0:
+                    raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class, BassGeometry child
+            :param BaseGeometry:
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
