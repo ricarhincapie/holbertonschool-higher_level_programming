c0c89395999792437583843ee0ac3ee83ce07060
Ricardo Hincapie <1516@holbertonschool.com>
2020-06-04 03:22:14 +0000

    Task #0 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
             :param filename="": file to read
     """
     with open(filename, encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
