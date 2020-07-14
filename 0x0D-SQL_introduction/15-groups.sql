0b8022335e6cc46885ee9564ba57d2b726192d27
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-07-14 03:47:56 +0000

    Add task #15 correction

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Counts and displays occurencies of a data point from a table
-SELECT score, COUNT(score) as number from second_table GROUP BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
