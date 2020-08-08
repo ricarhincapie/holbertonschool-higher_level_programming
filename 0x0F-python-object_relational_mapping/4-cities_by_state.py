71efa9d456b00db27455094a90b1bb46b938479c
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-08 21:19:34 +0000

    Add task #4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script returns all data from db"""
+import MySQLdb
+from sys import argv
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]  # Password: 1122
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    my_text = "SELECT c.id, c.name, s.name FROM cities c \
+               INNER JOIN states s on c.state_id = s.id"
+    cur.execute(my_text)  # Here, a tuple needs to be used
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
