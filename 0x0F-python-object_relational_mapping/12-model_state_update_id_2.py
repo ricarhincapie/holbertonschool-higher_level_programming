41faaeaa1642900ba946829775d716ae1aba42d6
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-10 00:25:38 +0000

    Add task #12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Shows all objects from tables states"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    to_update = session.query(State).filter(State.id == 2).first()
+    to_update.name = "New Mexico"
+    session.commit()
+
+    print(to_update.id, to_update.name)
+    session.close()
