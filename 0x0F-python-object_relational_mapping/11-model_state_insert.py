fbf3e785141e513bba9f37362f24a546d8b19cc8
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-10 00:13:14 +0000

    Add task #11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Shows all objects from tables states"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    insertion = State(name='Louisiana')
+    session.add(insertion)
+    session.commit()
+    print(insertion.id)
+    session.close()
