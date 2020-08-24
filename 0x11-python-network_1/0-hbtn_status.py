a71ceaa34b16c505748fa3737cc7d9d48aaa0b7b
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-24 19:55:28 +0000

    Add task #0 corrections

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: ", type(html))
-        print("\t- content: ", html)
-        print("\t- utf8 content: ", html.decode())
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode())
