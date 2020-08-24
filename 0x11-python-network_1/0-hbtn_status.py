30778296a1f9cb1deec1f2d59264099799429da9
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-24 19:48:10 +0000

    Add task #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches an URL and displays response body
+"""
+if __name__ == "__main__":
+    from urllib import request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: ", type(html))
+        print("\t- content: ", html)
+        print("\t- utf8 content: ", html.decode())
