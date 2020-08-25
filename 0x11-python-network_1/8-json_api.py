4950a26b9bb32d0bde1abb8b6040bbd7a6d90f31
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-25 14:46:59 +0000

    Add task #8 correction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
         q = str(sys.argv[1])
     else:
         q = ""
-    url = 'http://de0f2fdee31c.ff38d7df.hbtn-cod.io:5000/search_user'
+    url = 'http://0.0.0.0:5000/search_user'
     response = requests.post(url, data={'q': q})
     try:
         # response.raise_for_status()
         json_response = response.json()
-        if (len(json_response)) == 0 or not json_response:
+        if (len(json_response)) == 0:
             print("No result")
         if (len(json_response)) > 0:
             print("[{}] {}".format(json_response['id'], json_response['name']))
