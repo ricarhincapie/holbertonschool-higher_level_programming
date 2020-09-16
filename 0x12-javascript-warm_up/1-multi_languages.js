2ebf4d1063dd1ea3e074241a26e79c39bdae9fee
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-16 20:44:54 +0000

    Add task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'Javascript is amazing';
+
+const texts = [a, b, c];
+
+for (let i = 0; i < texts.length; i++) {
+  console.log(texts[i]);
+}
