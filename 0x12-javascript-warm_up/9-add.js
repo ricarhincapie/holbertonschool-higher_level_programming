6160bd1e43ceb65fd196b2326cc2b26c8b27dece
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-16 23:23:37 +0000

    Add task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const a = process.argv[2];
+const b = process.argv[3];
+
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  add(a, b);
+}
+
+function add (a, b) {
+  console.log(a * b);
+}
