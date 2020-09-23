fe36d7d94502902b7bb352a9aca8f776e54a5d7e
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-23 16:39:29 +0000

    Add task 3 upgrade

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,11 +4,11 @@ const myId = process.argv[2];
 
 const myUrl = 'https://swapi-api.hbtn.io/api/films/' + myId;
 
-request.get(myUrl, function (err, response, body) {
+request.get(myUrl, function (err, response, body) { /* Aparently it needs all args */
   if (err) {
     console.error(err);
   } else {
-    console.log(JSON.parse(body).title);
+    console.log(JSON.parse(body).title); /* Converts to JS object */
   }
 }
 );
