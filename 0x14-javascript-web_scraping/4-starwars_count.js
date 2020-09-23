f35943838608a4c7882a435aa1f4bd09ebd3d0c5
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-23 17:29:49 +0000

    Add task 4 correction

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const myUrl = process.argv[2];
-const myChar = 'https://swapi-api.hbtn.io/api/people/18/';
+const myChar = '18';
 let count = 0;
 
 request.get(myUrl, function (err, response, body) { /* Aparently it needs all args */
@@ -11,9 +11,12 @@ request.get(myUrl, function (err, response, body) { /* Aparently it needs all ar
     const jsonResponse = JSON.parse(body).results; /* Converts to JS object */
     /* console.log(jsonResponse.length) */
     /* console.log(JSON.stringify(jsonResponse)); */
-    for (let i = 0; i < jsonResponse.length; i++) {
-      if (jsonResponse[i].characters.includes(myChar)) { /* Includes method */
-        count++;
+
+    for (const film of jsonResponse) {
+      for (const string of film.characters) {
+        if (string.includes(myChar)) { /* Includes method */
+          count++;
+        }
       }
     }
     console.log(count);
